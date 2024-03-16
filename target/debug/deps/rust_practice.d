426bfa774b14555bb63444f5a34ab7316eaa30ac
vishalmet <vishalmet003@gmail.com>
2024-03-16 12:28:32 +0530

    first-prog

---
+++ target/debug/deps/rust_practice.d
@@ -0,0 +1,5 @@
+C:\Users\abhi\vishal\rust-practice\target\debug\deps\rust_practice.exe: src\main.rs
+
+C:\Users\abhi\vishal\rust-practice\target\debug\deps\rust_practice.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/rust-practice.d
@@ -0,0 +1,1 @@
+C:\Users\abhi\vishal\rust-practice\target\debug\rust-practice.exe: C:\Users\abhi\vishal\rust-practice\src\main.rs
